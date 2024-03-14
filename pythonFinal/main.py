a0307b298130d281231768c30d69a388081a601c
Chonnseht Phosawatmanee <chonnseht@gmail.com>
2024-03-14 09:43:03 +0700

    Final

---
+++ pythonFinal/main.py
@@ -0,0 +1,30 @@
+import Final.pythonclass as Q1
+import Final.case_badgenius as C1
+import Final.stack as Q2
+import Final.queue as Q3
+import Final.bubblesort as Q401
+import Final.quicksort as Q402
+import Final.dictsort as Q403
+import Final.montecarlo as Q5
+import Final.regex as Q6
+import Final.manhattan_euclidean as Q7
+
+if __name__ == '__main__':
+    Q1.run01()
+    #Q1.run02()
+    #Q1.run03()
+    #C1.run()
+    #Q2.run()
+    #Q3.run()
+    #Q401.run()
+    #Q402.run01()
+    #Q402.run02()
+    #Q403.run01()
+    #Q403.run02()
+    #Q5.run01()
+    #Q5.run02()
+    #Q5.run03()
+    #Q6.re01()
+    #Q6.re02()
+    #Q7.MD()
+    #Q7.ED()
